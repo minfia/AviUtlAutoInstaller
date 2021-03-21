0a655cfd687ae93c1abbd5cc763616bc1cc8ab2d
minfia <mintofia@gmail.com>
2021-03-21 17:50:39 +0900

    Change InstallEditView layout

--- AviUtlAutoInstaller/ViewModels/InstallEditViewModel.cs
+++ AviUtlAutoInstaller/ViewModels/InstallEditViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AviUtlAutoInstaller.ViewModels
 {
-    class InstallEditViewModel
+    class InstallEditViewModel : NotificationObject
     {
+        public InstallEditViewModel()
+        {
+        }
     }
 }
