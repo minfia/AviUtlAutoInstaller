d83644cc813db709d04d547dbf428f07d0abb8ca
minfia <mintofia@gmail.com>
2021-03-21 18:40:32 +0900

    Add InstallEditView

--- AviUtlAutoInstaller/ViewModels/InstallEditViewModel.cs
+++ AviUtlAutoInstaller/ViewModels/InstallEditViewModel.cs
@@ -9,8 +9,35 @@ namespace AviUtlAutoInstaller.ViewModels
 {
     class InstallEditViewModel : NotificationObject
     {
+        #region ユーザータブ
+        private DelegateCommand _addItemCommand;
+        private DelegateCommand _modifyItemCommand;
+        private DelegateCommand _deleteItemCommand;
+
+        public DelegateCommand AddItemCommand { get => _addItemCommand; }
+        public DelegateCommand ModifyItemCommand { get => _modifyItemCommand; }
+        public DelegateCommand DeleteItemCommand;
+        #endregion
+
+        #region InstallItemEditViewの設定
+        private InstallItemEditViewModel _installItemEditViewModel = new InstallItemEditViewModel();
+        public InstallItemEditViewModel InstallItemEditViewModel { get { return _installItemEditViewModel; } }
+        private Action<bool> _installItemEditViewCallback;
+        public Action<bool> InstallItemEditViewCallback
+        {
+            get { return _installItemEditViewCallback; }
+            private set { SetProperty(ref _installItemEditViewCallback, value); }
+        }
+        private void OnInstallItemEditView(bool result)
+        {
+            InstallItemEditViewCallback = null;
+        }
+        #endregion
+
         public InstallEditViewModel()
         {
+            _addItemCommand = new DelegateCommand(_ => InstallItemEditViewCallback = OnInstallItemEditView);
+            _modifyItemCommand = new DelegateCommand(_ => InstallItemEditViewCallback = OnInstallItemEditView);
         }
-    }
+   }
 }
