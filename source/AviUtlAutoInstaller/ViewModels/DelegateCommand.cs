0004ffdf9790a9c55f90b47b329fa7c98524cb6c
minfia <minfialab@gmail.com>
2021-04-11 16:31:48 +0900

    Add datagrid cell processing for right click and cell unfocus

--- source/AviUtlAutoInstaller/ViewModels/DelegateCommand.cs
+++ source/AviUtlAutoInstaller/ViewModels/DelegateCommand.cs
@@ -40,4 +40,40 @@ namespace AviUtlAutoInstaller.ViewModels
             }
         }
     }
+
+    class DelegateCommand<T> : ICommand
+    {
+        private Action<T> _execute;
+        private Func<T, bool> _canExecute;
+
+        public DelegateCommand(Action<T> exetute) : this(exetute, null)
+        {
+        }
+
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (_canExecute != null) ? _canExecute((T)parameter) : true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExetuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+            {
+                _execute((T)parameter);
+            }
+        }
+    }
 }
