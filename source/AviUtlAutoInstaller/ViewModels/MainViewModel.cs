666f8684c4e6df72ddddb9b447af8f1897a7525d
minfia <minfialab@gmail.com>
2021-04-17 12:50:22 +0900

    Change the show method calling from MainView

--- source/AviUtlAutoInstaller/ViewModels/MainViewModel.cs
+++ source/AviUtlAutoInstaller/ViewModels/MainViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using AviUtlAutoInstaller.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AviUtlAutoInstaller.ViewModels
 {
@@ -61,50 +63,6 @@ namespace AviUtlAutoInstaller.ViewModels
         }
         #endregion
 
-        #region インストール一覧の表示設定
-        private InstallEditViewModel _instalEditViewModel = new InstallEditViewModel();
-        public InstallEditViewModel InstallEditViewModel { get { return _instalEditViewModel; } }
-        private Action<bool> _installEditViewCallback;
-        public Action<bool> InstallEditViewCallback
-        {
-            get { return _installEditViewCallback; }
-            private set { SetProperty(ref _installEditViewCallback, value); }
-        }
-        private void OnInstallEditView(bool result)
-        {
-            InstallEditViewCallback = null;
-        }
-        #endregion
-
-        #region アップデートチェックの表示設定
-        private UpdateCheckViewModel _updateCheckViewModel = new UpdateCheckViewModel();
-        public UpdateCheckViewModel UpdateCheckViewModel { get { return _updateCheckViewModel; } }
-        private Action<bool> _updateCheckViewCallback;
-        public Action<bool> UpdateCheckViewCallback
-        {
-            get { return _updateCheckViewCallback; }
-            private set { SetProperty(ref _updateCheckViewCallback, value); }
-        }
-        private void OnUpdateCheckView(bool result)
-        {
-            UpdateCheckViewCallback = null;
-        }
-        #endregion
-
-        #region バージョン情報の表示設定
-        private AboutViewModel _aboutViewModel = new AboutViewModel();
-        public AboutViewModel AboutViewModel { get { return _aboutViewModel; } }
-        private Action<bool> _aboutViewCallback;
-        public Action<bool> AboutViewCallback
-        {
-            get { return _aboutViewCallback; }
-            private set { SetProperty(ref _aboutViewCallback, value); }
-        }
-        private void OnAboutView(bool result)
-        {
-            AboutViewCallback = null;
-        }
-        #endregion
         #endregion
 
         private readonly DelegateCommand selectInstallDir;
@@ -116,9 +74,27 @@ namespace AviUtlAutoInstaller.ViewModels
             _openDialogInstallSettingFileCommand = new DelegateCommand(_ => OpenDialogInstallSettingFileCallback = OnOpenDialogInstallSettingFileCallback);
             _openDialogUserRepoFileCommand = new DelegateCommand(_ => OpenDialogUserRepoFileCallback = OnOpenDialogUserRepoFileCallback);
             _exitCommand = new DelegateCommand(_ => OnExit());
-            _installEditCommand = new DelegateCommand(_ => InstallEditViewCallback = OnInstallEditView);
-            _updateCheckCommand = new DelegateCommand(_ => UpdateCheckViewCallback = OnUpdateCheckView);
-            _aboutCommand = new DelegateCommand(_ => AboutViewCallback = OnAboutView);
+            _installEditCommand = new DelegateCommand(
+                _ =>
+                {
+                    InstallEditView window = new InstallEditView();
+                    window.Owner = Application.Current.MainWindow;
+                    window.ShowDialog();
+                });
+            _updateCheckCommand = new DelegateCommand(
+                _ =>
+                {
+                    UpdateCheckView window = new UpdateCheckView();
+                    window.Owner = Application.Current.MainWindow;
+                    window.ShowDialog();
+                });
+            _aboutCommand = new DelegateCommand(
+                _ =>
+                {
+                    AboutView window = new AboutView();
+                    window.Owner = Application.Current.MainWindow;
+                    window.ShowDialog();
+                });
         }
     }
 }
