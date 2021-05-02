1991cdb5f2ccc3dea6f1a070ac3b89dd01791312
minfia <minfialab@gmail.com>
2021-05-02 20:54:41 +0900

    Add PSDToolKit install process

--- source/AviUtlAutoInstaller/Models/ItemInstallProc.cs
+++ source/AviUtlAutoInstaller/Models/ItemInstallProc.cs
@@ -59,6 +59,16 @@ namespace AviUtlAutoInstaller.Models
             return true;
         }
 
+        private enum SpecialPluginType
+        {
+            PSDToolkit,
+        };
+
+        private static readonly Dictionary<SpecialPluginType, string> _specialPluginDic = new Dictionary<SpecialPluginType, string>()
+        {
+            { SpecialPluginType.PSDToolkit, "PSDToolKit" },
+        };
+
         /// <summary>
         /// 単純にインストール出来ないプラグインの判定
         /// </summary>
@@ -66,17 +76,56 @@ namespace AviUtlAutoInstaller.Models
         /// <returns></returns>
         private bool IsSpecialPlugin(string name)
         {
-            return false;
+            bool b = _specialPluginDic.Any(x => x.Value == name);
+            return b;
         }
 
         /// <summary>
         /// 単純にインストール出来ないプラグインのインストール
         /// </summary>
-        /// <param name="name"></param>
+        /// <param name="name">名称</param>
         /// <returns></returns>
         private bool InstallSpecialPlugin(string name)
         {
+            var type = _specialPluginDic.First(x => x.Value == name).Key;
+
+            switch (type)
+            {
+                case SpecialPluginType.PSDToolkit:
+                    InstallPSDToolKit();
+                    break;
+                default:
+                    return false;
+            }
+
             return true;
+        }
+
+        /// <summary>
+        /// PSDToolKitのインストール
+        /// </summary>
+        private void InstallPSDToolKit()
+        {
+            FileOperation fileOperation = new FileOperation();
+            string psdSrcPath = $"{SysConfig.InstallExpansionDir}\\{Path.GetFileNameWithoutExtension(DownloadFileName)}";
+
+            // 取説の移動
+            string psdManualDestPath = $"{SysConfig.InstallRootPath}\\PSDToolKitの説明ファイル群";
+            Directory.CreateDirectory(psdManualDestPath);
+
+            string srcDocsPath = $"{psdSrcPath}\\PSDToolKitDocs";
+            fileOperation.DirectoryMove(srcDocsPath, $"{psdManualDestPath}\\PSDToolKitDocs");
+            Directory.Delete(srcDocsPath, true);
+            string[] fileNames = { "GCMZDrops.txt", "PSDToolKit.txt", "PSDToolKit説明書.html", "ZRamPreview.txt", "キャッシュテキスト.txt" };
+            List<string> srcFileNamePath = new List<string>();
+            foreach (string fileName in fileNames)
+            {
+                srcFileNamePath.Add($"{psdSrcPath}\\{fileName}");
+            }
+            fileOperation.FileMove(srcFileNamePath.ToArray(), psdManualDestPath);
+
+            // 本体の移動
+            fileOperation.DirectoryMove($"{SysConfig.InstallExpansionDir}\\{Path.GetFileNameWithoutExtension(DownloadFileName)}", $"{SysConfig.AviUtlPluginDir}");
         }
     }
 }
