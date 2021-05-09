f8053bccf00581bf8f0ce36b508b07376061f1c7
minfia <minfialab@gmail.com>
2021-05-09 22:46:11 +0900

    Add AboutView and application icon

---
+++ source/AviUtlAutoInstaller/Models/ProductInfo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AviUtlAutoInstaller.Models
+{
+    static class ProductInfo
+    {
+        private static Assembly assembly = Assembly.GetExecutingAssembly();
+
+        private static readonly string _appName = assembly.GetName().Name;
+        /// <summary>
+        /// アプリケーション名
+        /// </summary>
+        public static string AppName
+        {
+            get { return _appName; }
+        }
+
+        private static readonly string _appVersion = assembly.GetName().Version.ToString();
+        /// <summary>
+        /// アプリバージョン
+        /// </summary>
+        public static string AppVersion
+        {
+            get { return _appVersion; }
+        }
+
+        private static readonly string _description = ((AssemblyDescriptionAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyDescriptionAttribute))).Description;
+        /// <summary>
+        /// アプリの説明
+        /// </summary>
+        public static string Desctiption
+        {
+            get { return _description; }
+        }
+
+        private static readonly string _company = ((AssemblyCompanyAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCompanyAttribute))).Company;
+        /// <summary>
+        /// 会社名
+        /// </summary>
+        public static string Company
+        {
+            get { return _company; }
+        }
+
+        private static readonly string _product = ((AssemblyProductAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyProductAttribute))).Product;
+        /// <summary>
+        /// プロダクト名
+        /// </summary>
+        public static string Product
+        {
+            get { return _product; }
+        }
+
+        private static readonly string _copyright = ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCopyrightAttribute))).Copyright;
+        /// <summary>
+        /// コピーライト
+        /// </summary>
+        public static string Copyright
+        {
+            get { return _copyright; }
+        }
+    }
+}
--- source/AviUtlAutoInstaller/ViewModels/AboutViewModel.cs
+++ source/AviUtlAutoInstaller/ViewModels/AboutViewModel.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using AviUtlAutoInstaller.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace AviUtlAutoInstaller.ViewModels
 {
-    class AboutViewModel
+    class AboutViewModel : NotificationObject
     {
+        public string ApplicationName
+        {
+            get { return ProductInfo.AppName; }
+        }
+
+        /// <summary>
+        /// アプリバージョン
+        /// </summary>
+        public string ApplicationVersion
+        {
+            get { return ProductInfo.AppVersion; }
+        }
+
+        private string _preRepoVersion;
+        /// <summary>
+        /// プリインストールのバージョン
+        /// </summary>
+        public string PreRepoVersion
+        {
+            get { return _preRepoVersion; }
+            private set { SetProperty(ref _preRepoVersion, value); }
+        }
+
+        private uint MajorVersion { get; set; }
+        private uint MinorVersion { get; set; }
+
+        public AboutViewModel()
+        {
+            PreRepoFileR preRepoFileR = new PreRepoFileR($"{SysConfig.RepoDirPath}\\aai.repo");
+            preRepoFileR.Open();
+            preRepoFileR.GetDBVersion(out uint major, out uint minor);
+            preRepoFileR.Close();
+            MajorVersion = major;
+            MinorVersion = minor;
+            PreRepoVersion = $"{MajorVersion}.{MinorVersion}";
+        }
+
+        public void OpenLink(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Hyperlink hyperLink = (Hyperlink)sender;
+            System.Diagnostics.Process.Start(hyperLink.NavigateUri.ToString());
+        }
     }
 }
