d0fb807798a5486869b63c28904c7d81a06c2a7d
minfia <mintofia@gmail.com>
2021-08-15 20:21:24 +0900

    Fix encoder uninstall process

--- source/AviUtlAutoInstaller/Models/ItemInstallProc.cs
+++ source/AviUtlAutoInstaller/Models/ItemInstallProc.cs
@@ -73,37 +73,46 @@ namespace AviUtlAutoInstaller.Models
         {
             string baseDir = $"{SysConfig.InstallRootPath}";
 
-            switch (item.FileType)
+            if (InstallFileType.Encoder == item.FileType)
             {
-                case InstallFileType.Tool:
-                    baseDir = $"{SysConfig.InstallRootPath}";
-                    break;
-                case InstallFileType.Plugin:
-                    if (item.IsSpecialItem)
-                    {
-                        return UninstallSpecialPlugin(item);
-                    }
-                    else
-                    {
-                        baseDir = $"{SysConfig.AviUtlPluginDir}";
-                    }
-                    break;
-                case InstallFileType.Script:
-                    baseDir = $"{SysConfig.AviUtlScriptDir}";
-                    break;
-            }
+                UninstallRigayaEncoder(item);
 
-            FileOperation fileOperation = new FileOperation();
-            List<string> uninstallFileList = fileOperation.GenerateFilePathList(baseDir, item.InstallFileList.ToArray());
-            foreach (var file in uninstallFileList)
+                return true;
+            }
+            else
             {
-                try
+                switch (item.FileType)
                 {
-                    File.Delete(file);
+                    case InstallFileType.Tool:
+                        baseDir = $"{SysConfig.InstallRootPath}";
+                        break;
+                    case InstallFileType.Plugin:
+                        if (item.IsSpecialItem)
+                        {
+                            return UninstallSpecialPlugin(item);
+                        }
+                        else
+                        {
+                            baseDir = $"{SysConfig.AviUtlPluginDir}";
+                        }
+                        break;
+                    case InstallFileType.Script:
+                        baseDir = $"{SysConfig.AviUtlScriptDir}";
+                        break;
                 }
-                catch
+
+                FileOperation fileOperation = new FileOperation();
+                List<string> uninstallFileList = fileOperation.GenerateFilePathList(baseDir, item.InstallFileList.ToArray());
+                foreach (var file in uninstallFileList)
                 {
-                    return false;
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch
+                    {
+                        return false;
+                    }
                 }
             }
 
@@ -418,6 +427,103 @@ namespace AviUtlAutoInstaller.Models
                 {
                     Directory.Delete(extractDirPath, true);
                 }
+            }
+        }
+
+        private static void UninstallRigayaEncoder(InstallItem item)
+        {
+            string delEncName;
+
+            if (item.Name.Equals("x264guiEx"))
+            {
+                // 削除対象
+                // \exe_files\boxdumper.exe
+                // \exe_files\ffmpeg_audenc.exe
+                // \exe_files\muxer.exe
+                // \exe_files\remuxer.exe
+                // \exe_files\timelineeditor.exe
+                // \exe_files\x264_*.exe
+                // \plugins\x264guiEx_stg
+                // \plugins\x264guiEx.auo
+                // \plugins\x264guiEx.conf
+                // \plugins\x264guiEx.ini
+
+                delEncName = "x264guiEx";
+            }
+            else if (item.Name.Equals("QSVEnc"))
+            {
+                // 削除対象
+                // \exe_files\QSVEncC
+                // \plugins\QSVEnc_stg
+                // \plugins\QSVEnc.auo
+                // \plugins\QSVEnc.conf
+                // \plugins\QSVEnc.ini
+
+                delEncName = "QSVEnc";
+            }
+            else if (item.Name.Equals("NVEnc"))
+            {
+                // 削除対象
+                // \exe_files\NVEncC
+                // \plugins\NVEnc_stg
+                // \plugins\NVEnc.auo
+                // \plugins\NVEnc.conf
+                // \plugins\NVEnc.ini
+
+                delEncName = "NVEnc";
+            }
+            else if (item.Name.Equals("VCEEnc"))
+            {
+                // 削除対象
+                // \exe_files\VCEEncC
+                // \plugins\VCEEnc_stg
+                // \plugins\VCEEnc.auo
+                // \plugins\VCEEnc.conf
+                // \plugins\VCEEnc.ini
+
+                delEncName = "VCEEnc";
+            }
+            else
+            {
+                return;
+            }
+
+            FileOperation fileOperation = new FileOperation();
+            string exe_filesDir = $"{SysConfig.InstallRootPath}\\exe_files";
+
+            {
+                // exe_files配下のファイル/ディレクトリ削除
+                if (item.Name.Equals("x264guiEx"))
+                {
+                    string[] files = { "boxdumper.exe", "ffmpeg_audenc.exe", "muxer.exe", "remuxer.exe", "timelineeditor.exe", "x264_*.exe" };
+                    var delFiles = fileOperation.GenerateFilePathList(exe_filesDir, files).ToArray();
+
+                    foreach (string delFile in delFiles)
+                    {
+                        File.Delete(delFile);
+                    }
+                }
+                else
+                {
+                    Directory.Delete($"{exe_filesDir}\\{delEncName}C", true);
+                }
+            }
+
+            {
+                // plugins配下のファイル/ディレクトリ削除
+                Directory.Delete($"{SysConfig.AviUtlPluginDir}\\{delEncName}_stg", true);
+                string[] files = { $"{delEncName}.*" };
+                var delFiles = fileOperation.GenerateFilePathList(SysConfig.AviUtlPluginDir, files).ToArray();
+
+                foreach (string delFile in delFiles)
+                {
+                    File.Delete(delFile);
+                }
+            }
+
+            if (fileOperation.IsDirectoryEmpty(exe_filesDir))
+            {
+                Directory.Delete(exe_filesDir);
             }
         }
 
