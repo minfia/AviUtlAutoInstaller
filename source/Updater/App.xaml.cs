ad6cd5d9bd565d66cdf7de5eaf443b9c02516297
minfia <mintofia@gmail.com>
2021-10-14 11:46:11 +0900

    Add: Updater base

---
+++ source/Updater/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using AAIUpdater.ViewModels;
+using AAIUpdater.Views;
+
+namespace AAIUpdater
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mv = new MainView();
+            var mvm = new MainViewModel();
+
+            mv.DataContext = mvm;
+            mv.Show();
+        }
+    }
+}
